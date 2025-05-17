a1b74f73ab96562323eede0d9efe0ded549cc766
LAK0028 <petr.lakomy.st@vsb.cz>
2025-05-17 11:49:06 +0000

    Created functionality for animations.

--- processing.py
+++ processing.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import json
 import matplotlib.pyplot as plt
+import imageio.v2 as imageio
+from natsort import natsorted
 
 
 class ReadInputData:
@@ -66,8 +68,43 @@ class Mesh:
         self.create_mesh_plot(matrix, title, bar_title, min, max)
         plt.show()
 
-    def save_results(self, matrix, title="title", bar_title="bar_title",
-                     min=None, max=None, save_path="plot.png"):
+    def save_result(self, matrix, title="title", bar_title="bar_title",
+                    min=None, max=None, save_path="plot.png"):
         fig = self.create_mesh_plot(matrix, title, bar_title, min, max)
         fig.savefig(save_path, dpi=300)
         plt.close(fig)
+
+    def save_sequence_of_results(self, matrix_dict, title="title", bar_title="bar_title",
+                                 min=None, max=None, save_path="results"):
+        os.makedirs(save_path)
+
+        for time_key in sorted(matrix_dict.keys()):
+            matrix = matrix_dict[time_key]
+            file_title = f"{title} {time_key}s"
+            file_name = f"{save_path}/{time_key:06.0f}.png"
+
+            self.save_result(matrix, title=file_title, bar_title=bar_title,
+                             min=min, max=max, save_path=file_name)
+
+
+def create_video_from_sequence(folder_path, output_path, fps=1):
+    """
+    Creates a video from images in a folder.
+
+    Args:
+        folder_path (str): Path to the folder containing image files.
+        output_path (str): Path where the output video will be saved.
+        fps (int): Frames per second for the output video.
+
+    Returns:
+        None
+    """
+    files = natsorted(os.listdir(folder_path))
+
+    images = []
+    for file in files:
+        file_path = os.path.join(folder_path, file)
+        if os.path.isfile(file_path):
+            images.append(imageio.imread(file_path))
+
+    imageio.mimsave(output_path, images, fps=fps, codec="libx264")
